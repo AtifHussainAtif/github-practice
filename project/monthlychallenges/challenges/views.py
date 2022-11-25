f44f50b2849dbf50e482259ca7f79ca8cea955ad
Atif Hussain <atifhussain1320@gmail.com>
2022-11-25 14:39:45 +0500

    worked on views and urls

--- project/monthlychallenges/challenges/views.py
+++ project/monthlychallenges/challenges/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+def january(request):
+    return HttpResponse("This Month is january")
+
+def february(request):
+    return HttpResponse("this month is february")
