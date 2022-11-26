473244cfdb17dc0b30f1f74d9dd3c0b472811bb7
Atif Hussain <atifhussain1320@gmail.com>
2022-11-26 12:31:57 +0500

    changes made to views and urls

--- project/monthlychallenges/challenges/views.py
+++ project/monthlychallenges/challenges/views.py
@@ -1,13 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
-def january(request):
-    return HttpResponse("This Month is january")
+monthly_challenges = {
+    "january" : "this is january",
+    "february": "this is february",
+    "march": "this is march",
+    "april" : "this is april",
+    "may" : "this is may",
+    "june" : "this is june",
+    "july" : "this is june",
+    "july" : "this is july",
+    "august" : "this is august",
+    "september" : "this is september",
+    "october" : "this is october",
+    "november" : "this is november",
+    "december" : "this is december"
+}
 
-def february(request):
-    return HttpResponse("this month is february")
+
+
+def monthly_challenges_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("invalid month")
+
+    redirect_month = months[month-1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
-    return HttpResponse()
+    try:
+       challenges_text = monthly_challenges[month]
+       return HttpResponse(challenges_text)
+    
+    except:
+        return HttpResponseNotFound("wrong month")
